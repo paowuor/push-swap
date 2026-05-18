242fa91baf071d5d03ea76d2291e86c9b0881a90
paowuor <owuorpaul500@gmail.com>
2026-05-18 15:04:38 +0300

    Created the push-swap logic

---
+++ push-swap/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+
+	"paowuor-push-swap/internal/parser"
+	"paowuor-push-swap/internal/utils"
+)
+
+func main() {
+	numbers, err := parser.ParseArguments(os.Args[1:])
+	if err != nil {
+		utils.PrintError()
+		return
+	}
+
+	if len(numbers) == 0 {
+		return
+	}
+
+	if utils.IsSorted(numbers) {
+		return
+	}
+}
