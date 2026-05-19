0d05c167da08226f8e6e8bf95b62ef4b9c309935
paowuor <owuorpaul500@gmail.com>
2026-05-19 09:47:29 +0300

    Created push operations

---
+++ internal/stack/push.go
@@ -0,0 +1,33 @@
+package stack
+
+import "paowuor-push-swap/internal/models"
+
+func Pa(a, b *models.Stack, ops *[]string) {
+	if len(b.Values) == 0 {
+		return
+	}
+
+	value := b.Values[0]
+	b.Values = b.Values[1:]
+
+	a.Values = append([]int{value}, a.Values...)
+
+	if ops != nil {
+		*ops = append(*ops, "pa")
+	}
+}
+
+func Pb(a, b *models.Stack, ops *[]string) {
+	if len(a.Values) == 0 {
+		return
+	}
+
+	value := a.Values[0]
+	a.Values = a.Values[1:]
+
+	b.Values = append([]int{value}, b.Values...)
+
+	if ops != nil {
+		*ops = append(*ops, "pb")
+	}
+}
