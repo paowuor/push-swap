446c58d3fff7bd20e18ef9ac6b80765e9627787c
paowuor <owuorpaul500@gmail.com>
2026-05-19 10:15:13 +0300

    Created rotate operations

---
+++ internal/stack/rotate.go
@@ -0,0 +1,38 @@
+package stack
+
+import "paowuor-push-swap/internal/models"
+
+func Ra(a *models.Stack, ops *[]string) {
+	if len(a.Values) < 2 {
+		return
+	}
+
+	first := a.Values[0]
+	a.Values = append(a.Values[1:], first)
+
+	if ops != nil {
+		*ops = append(*ops, "ra")
+	}
+}
+
+func Rb(b *models.Stack, ops *[]string) {
+	if len(b.Values) < 2 {
+		return
+	}
+
+	first := b.Values[0]
+	b.Values = append(b.Values[1:], first)
+
+	if ops != nil {
+		*ops = append(*ops, "rb")
+	}
+}
+
+func Rr(a, b *models.Stack, ops *[]string) {
+	Ra(a, nil)
+	Rb(b, nil)
+
+	if ops != nil {
+		*ops = append(*ops, "rr")
+	}
+}
