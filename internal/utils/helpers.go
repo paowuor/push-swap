aad2f41b72cdbb1fb1010bdd8727d84984b80f79
paowuor <owuorpaul500@gmail.com>
2026-05-20 10:43:36 +0300

    Updated the helper functions to check if stack is sorted

--- internal/utils/helpers.go
+++ internal/utils/helpers.go
@@ -8,3 +8,13 @@ func IsSorted(numbers []int) bool {
 	}
 	return true
 }
+
+func IsStackSorted(stack []int) bool {
+	for i := 0; i < len(stack)-1; i++ {
+		if stack[i] > stack[i+1] {
+			return false
+		}
+	}
+
+	return true
+}
