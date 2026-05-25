fa69d9e8c0e36cd0b1a4245535ebef10198b4152
Paul Owuor <owuorpaul500@gmail.com>
2026-05-25 09:04:35 +0000

    Add SortTwo function to sort two elements in a stack

---
+++ internal/algorithms/sort_two.go
@@ -0,0 +1,15 @@
+package algorithms
+
+import (
+	"push-swap/internal/models"
+	"push-swap/internal/stack"
+)
+
+func SortTwo(a *models.Stack, ops *[]string) {
+	if len(a.Values) < 2 {
+		return
+	}
+
+	if a.Values[0] > a.Values[1] {
+		stack.Sa(a, ops)
+}
